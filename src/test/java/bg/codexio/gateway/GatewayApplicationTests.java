1a34f5b8682250542333a8c76d408ff7b607fea2
Георги Сираков <georgi.sirakov74@gmail.com>
2021-01-18 11:51:04 +0200

    initial commit

---
+++ src/test/java/bg/codexio/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.codexio.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
